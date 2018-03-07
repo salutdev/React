1d58f47fe0e912354f6a5f03210e189e6d784ae0
salutdev <salutsirius@gmail.com>
2018-03-07 15:29:02 -0800

    final

--- src/index.js
+++ src/index.js
@@ -1,44 +1,22 @@
-import './stylesheets/ui.scss'
-import './stylesheets/index.scss'
 import React from 'react'
 import { render } from 'react-dom'
-import { hello, goodbye } from './lib'
-import  { SkiDayList }  from './components/SkiDayList'
-//import  { SkiDayCount }  from './components/SkiDayCount-createReactClass'
-//import  { SkiDayCount }  from './components/SkiDayCount-ES6'
-import  { SkiDayCount }  from './components/SkiDayCount'
-import { App } from './components/App'
-//import { Router, Route, hashHistory } from 'react-router'
-import { HashRouter, Route, Link, Switch } from 'react-router-dom'
-import { Whoops404 } from './components/Whoops404'
-import { AddDayForm } from './components/AddDayFormStateless';
-
+import { Member } from './components/ui/Member'
+import { MemberList } from './components/ui/MemberList'
 
 window.React = React
 
-
-// render(
-
-//     <SkiDayCount />,
-//     document.getElementById('react-container')
-// )
-
 render(
 
-    <HashRouter>
-      <Switch>
-        <Route exact path="/" component={App} />
-        <Route exact path="/list-days" component={App}></Route>
-        <Route exact path="/list-days/:filter"  component={App} />
-        <Route exact path="/add-day" component={App} />
-        <Route component={Whoops404} />
-      </Switch>
-   </HashRouter>,
+    <div>
+        <Member admin={true} 
+                name="Edna Welch"
+                email="edna.welch88@example.com"
+                thumbnail="https://randomuser.me/api/portraits/women/3.jpg"
+                makeAdmin={(email) => console.log(email)} />
+        
+        <MemberList />
+    </div>
+            ,
+            document.getElementById('react-container')
 
-    // <Router history={hashHistory}>
-    //     <Route path="/" component={App} />
-    //     <Route path="*" component={Whoops404} />
-    // </Router>,
-    // <SkiDayCount />,
-    document.getElementById('react-container')
 )

74fafb283485819ae77d45c8b058c8d528e612d9
salutdev <salutsirius@gmail.com>
2019-02-18 23:26:38 -0800

    Change width

--- my-app/src/Constants.js
+++ my-app/src/Constants.js
@@ -1,4 +1,4 @@
 export const INCREMENT = 'INCREMENT'
 export const MAKE_MOVE = 'MAKE_MOVE'
-export const BOARD_SIZE = 3
-export const BOARD_WIDTH = 300
+export const BOARD_SIZE = 5
+export const BOARD_WIDTH = 400

91d0584efa906c3a02b92b59acbe6b1c6c1a235f
salutdev <salutsirius@gmail.com>
2019-03-15 17:30:32 -0700

    Use copying by Immutable js

--- my-app/src/reducers/boardStateReducer.js
+++ my-app/src/reducers/boardStateReducer.js
@@ -1,11 +1,12 @@
 import { MAKE_MOVE, BOARD_SIZE, BOARD_WIDTH} from '../Constants'
+import Immutable, { fromJS, setIn } from 'immutable'
 
 export default (boardState, action) => {
     const {type, payload} = action
     boardState = boardState || { board: [...new Array(BOARD_SIZE)].map(e => new Array(BOARD_SIZE).fill(0)), turn: 1, result: null }
 
     switch(type) {  
-        case MAKE_MOVE: return getNewBoardState(boardState, payload)
+        case MAKE_MOVE: return getNewBoardState2(boardState, payload)
         default: return boardState
     }
 }
@@ -28,7 +29,7 @@ function getNewBoardState(boardState, payload) {
         }
 
         newBoard[arY][arX] = boardState.turn
-
+ 
         const newResult = whoWonGame(newBoard, arX, arY)
         if (newResult) {
             newResult.winner = boardState.turn
@@ -41,6 +42,34 @@ function getNewBoardState(boardState, payload) {
     return boardState
 }
 
+function getNewBoardState2(boardState, payload) {
+    const {x, y} = payload
+    let newTurn = boardState.turn
+
+    const cellWidth = BOARD_WIDTH / BOARD_SIZE
+
+    const arX = parseInt(x / cellWidth) 
+    const arY = parseInt(y / cellWidth) 
+
+    if (boardState.board[arY][arX] === 0) {
+
+        let newBoard = fromJS(boardState.board)
+        newBoard = newBoard.setIn([arY, arX], boardState.turn)
+ 
+        const newResult = whoWonGame(newBoard, arX, arY)
+        if (newResult) {
+            newResult.winner = boardState.turn
+        }
+
+        newTurn = boardState.turn === 1 ? -1 : 1
+
+        let newPlainBoard = newBoard.toArray().map(a => a.toArray())
+
+        return { board: newPlainBoard, turn: newTurn, result: newResult }
+    }
+    return boardState
+}
+
 function whoWonGame(newBoard, arX, arY) {
     let sumX = 0
     let sumY = 0
@@ -48,15 +77,15 @@ function whoWonGame(newBoard, arX, arY) {
     let sumD2 = 0
     let result = null;
     for (let i = 0; i < BOARD_SIZE; i++) {
-        sumY += newBoard[arY][i]
-        sumX += newBoard[i][arX]
+        sumY += newBoard.getIn([arY, i])
+        sumX += newBoard.getIn([i, arX])
         
         if (arX === arY) {
-            sumD1 += newBoard[i][i]
+            sumD1 += newBoard.getIn([i, i])
         }
 
         if (arX === BOARD_SIZE - arY - 1) {
-            sumD2 += newBoard[i][BOARD_SIZE-i-1]
+            sumD2 += newBoard.getIn([i, BOARD_SIZE-i-1])
         }
     }
 
